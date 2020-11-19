62fc4b80b1a00ff5c01eaa0b42080f7568f80f1a
DTin98 <kaisin1505@gmail.com>
2020-11-19 16:03:01 +0700

    add navigations of screens

---
+++ src/scenes/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = ({params}) => (
+  <View>
+    <Text style={{fontSize: 30, color: 'red'}}>Home</Text>
+  </View>
+);
+
+export default Home;
