fe1411f51b81f4e17df33c750f28554627a50f94
ngochien88 <57312147+ngochien88@users.noreply.github.com>
2020-11-19 14:47:12 +0700

    khoitaomap_addsearch

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+//import App from './App';
 import {name as appName} from './app.json';
 import App from './src';
+import Mapview from './src/components/mapview';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Mapview);
