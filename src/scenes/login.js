bea595ae430b771d8f22fecfa2ac893c91f43da4
tumoqun <32707186+tumoqun@users.noreply.github.com>
2020-11-21 15:11:51 +0700

    image

--- src/scenes/login.js
+++ src/scenes/login.js
@@ -1,63 +1,44 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  Dimensions,
-} from 'react-native';
-import {TouchableNativeFeedback} from 'react-native-gesture-handler';
-import {PRIMARY, SECONDARY} from '_styles/color';
-import Icon from 'react-native-vector-icons/AntDesign';
+import React from 'react'
+import { StyleSheet, Text, View, Button, Image, Dimensions } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
 
-const W = Dimensions.get('screen').width;
 
-const Login = ({params}) => (
-  <View style={styles.container}>
-    <Text>Icon</Text>
-    <View style={styles.formLogin}>
-      <Text>Chao mung!</Text>
-      <View style={styles.input}>
-        <Icon name="user" size={30} color="black" />
-        <TextInput placeholder="Ten dang nhap hoac email"></TextInput>
-      </View>
-      <View style={styles.input}>
-        <Icon name="lock" size={30} color="black" />
-        <TextInput placeholder="Mat khau"></TextInput>
-      </View>
+const W = Dimensions.get("screen").width;
 
-      <View style={styles.option}>
-        <Text>Dang ky</Text>
-        <Text>Quen mat khau</Text>
-      </View>
+const Login = () => {
+    return (
+        <LinearGradient colors={['rgba(2, 16, 72, 1)',
+            'rgba(7, 23, 89, 0.8139)',
+            'rgba(14, 33, 110, 0.5871)',
+            'rgba(23, 46, 141, 0.2417)',
+            'rgba(30, 56, 163, 0)']} style={styles.container}>
+            <View style={styles.logo}>
+                <Image  source={require("../assets/img/logo.png")}></Image>
+            </View>
+            <View style={styles.form_login}>
+                <Text style={{fontSize: 24, fontWeight: "bold"}}>Chào mừng!</Text>
+            </View>
+        </LinearGradient>
+    )
+}
 
-      <TouchableNativeFeedback>
-        <Text>Dang nhap</Text>
-      </TouchableNativeFeedback>
-    </View>
-  </View>
-);
+export default Login
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: PRIMARY,
-    justifyContent: 'center',
-  },
-  formLogin: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    alignItems: 'center',
-  },
-  option: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-  },
-  input: {
-    flexDirection: 'row',
-  },
-});
+    container:{
+        flex:1,
+        alignItems: 'center'
+    },
+    logo:{
+        textAlign: 'center',
+        justifyContent: "center",
+    },
+    form_login: {
+        width: 0.9*W,
+        height: 0.9*W,
+        backgroundColor: 'white',
+        borderRadius: 30,
+        alignItems: 'center'
+    }
 
-export default Login;
+})
